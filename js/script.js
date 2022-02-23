fe5b423410c5ffc4c714153850f993debf6f83b0
Leanid <leanidpazniak@gmail.com>
2022-02-23 16:58:11 +0200

    movies changed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 console.log(numberOfFilms);
 
 const personalMovieDB = {
@@ -9,3 +9,13 @@ const personalMovieDB = {
     genres:[], 
     private: false
 };
+
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
+      scoreOfLastFilm1 = prompt('На сколько его оцените?', ''),
+      lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
+      scoreOfLastFilm2 = prompt('На сколько его оцените?', '');
+
+
+personalMovieDB.movies[lastFilm1] = scoreOfLastFilm1;
+personalMovieDB.movies[lastFilm2] = scoreOfLastFilm2;
+console.log(personalMovieDB);
