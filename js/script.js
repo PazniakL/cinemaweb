24b6e3cdf1491d0be7a9f211ae9d0c6d7c943318
Leanid <leanidpazniak@gmail.com>
2022-02-24 18:51:28 +0200

    questions modified

--- js/script.js
+++ js/script.js
@@ -4,18 +4,17 @@ console.log(numberOfFilms);
 const personalMovieDB = {
 
     count: numberOfFilms,
-    movies:{},
-    actors: {}, 
-    genres:[], 
+    movies: {},
+    actors: {},
+    genres: [],
     private: false
 };
 
-const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
-      scoreOfLastFilm1 = prompt('На сколько его оцените?', ''),
-      lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-      scoreOfLastFilm2 = prompt('На сколько его оцените?', '');
+for (let i = 1; i < 3; i++) {
+    const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
+        scoreOfLastFilm1 = prompt('На сколько его оцените?', '');
+        personalMovieDB.movies[lastFilm1] = scoreOfLastFilm1;
+}
 
 
-personalMovieDB.movies[lastFilm1] = scoreOfLastFilm1;
-personalMovieDB.movies[lastFilm2] = scoreOfLastFilm2;
 console.log(personalMovieDB);
